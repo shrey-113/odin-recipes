d56aa479bb16bde1127c2b1239fa8bf26abf07de
Shrey-113 <shreyanshngp11@gmail.com>
2022-12-26 23:56:42 +0530

    added ui to rps

--- rockpaper/script.js
+++ rockpaper/script.js
@@ -1,6 +1,28 @@
-const button = document.querySelector('#playbutton');
-button.addEventListener('click', () => {
-    game();
+const div = document.querySelector('#Result');
+const head1 = document.createElement('H1');
+
+const button1 = document.querySelector('#Rock');
+button1.addEventListener('click', () => {
+    const comp = getComputerChoice();
+    const playa = 'Rock';
+
+    rockPaperScissors(comp, playa);
+});
+
+const button2 = document.querySelector('#Paper');
+button2.addEventListener('click', () => {
+    const comp = getComputerChoice();
+    const playa = 'Paper';
+
+    rockPaperScissors(comp, playa);
+});
+
+const button3 = document.querySelector('#Scissors');
+button3.addEventListener('click', () => {
+    const comp = getComputerChoice();
+    const playa = 'Scissors';
+
+    rockPaperScissors(comp, playa);
 });
 
 function getComputerChoice(){
@@ -22,54 +44,26 @@ function getComputerChoice(){
 
 function rockPaperScissors(computerChoice, playerChoice){
     if(computerChoice == 'Rock' && playerChoice == 'Paper'){
-        console.log("You Win!! Paper Beats Rock!");
-        return 1;
+        head1.textContent = "You Win!! Paper Beats Rock!";
     }
     else if(playerChoice == 'Rock' && computerChoice == 'Paper'){
-        console.log("You Lose!! Paper Beats Rock!");
-        return 0;
+        head1.textContent = "You Lose!! Paper Beats Rock!";
     }
     else if(playerChoice == 'Paper' && computerChoice == 'Scissors'){
-        console.log("You Lose!! Scissors Beats Paper!");
-        return 0;
+        head1.textContent = "You Lose!! Scissors Beats Paper!";
     }
     else if(computerChoice == 'Paper' && playerChoice == 'Scissors'){
-        console.log("You Win!! Scissors Beats Paper!");
-        return 1;
+        head1.textContent = "You Win!! Scissors Beats Paper!";
     }
     else if(playerChoice == 'Rock' && computerChoice == 'Scissors'){
-        console.log("You Win!! Rock Beats Scissors!");
-        return 1;
+        head1.textContent = "You Win!! Rock Beats Scissors!";
     }
     else if(computerChoice == 'Rock' && playerChoice == 'Scissors'){
-        console.log("You Win!! Rock Beats Scissors!");
-        return 0;
+        head1.textContent = "You Lose!! Rock Beats Scissors!";
     }
     else{
-        console.log("It's a tie! a point to both!")
-        return 2;
-    }
-}
-
-function game(){
-
-    var points = 0;
-
-    for(var i =0; i<5; i++){
-        const comp = getComputerChoice();
-        const playa = window.prompt("Enter Rock, Paper or Scissors");
-
-        var a = rockPaperScissors(comp, playa);
-        var tie =0;
-
-        if(a==1){
-            points++;
-        }
-        if(a==2){
-            points++;
-            tie++;
-        }
+        head1.textContent = "It's a tie! a point to both!";
     }
 
-    console.log(`Your Points: ${points}, Computer's Points: ${(6-points+tie)}`);
+    div.appendChild(head1);
 }
